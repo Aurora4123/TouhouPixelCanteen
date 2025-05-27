8ea8680f42df5f58016e17d61802440b2a88734f
79328272 <HatsukiYukina@users.noreply.github.com>
2025-05-28 01:33:07 +0800

    添加了对农夫乐事，minecraft原版的tooltip添加支持，添加了大量食材的描述。

--- src/main/java/fun/aurora4123/tpc/tips/ToolTipHandler.java
+++ src/main/java/fun/aurora4123/tpc/tips/ToolTipHandler.java
@@ -5,9 +5,13 @@ import fun.aurora4123.tpc.initial.Tabs;
 import fun.aurora4123.tpc.initial.items.MItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import vectorwing.farmersdelight.common.registry.ModItems;
+
 
 
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.FORGE)
@@ -39,6 +43,58 @@ public class ToolTipHandler {
         if(event.getItemStack().getItem() == MItems.EGG_CUSTARD.get()){
             event.getToolTip().add(Component.translatable("tooltip.touhoupixelcanteen.egg_custard"));
         }
+        //下面是对农夫乐事食物进行的追加说明
+        if(event.getItemStack().getItem() == ModItems.TOMATO.get()){
+            event.getToolTip().add(Component.translatable("tooltip.farmersdelight.tomato"));
+        }
+        if(event.getItemStack().getItem() == ModItems.COOKING_POT.get()){
+            event.getToolTip().add(Component.translatable("tooltip.farmersdelight.cooking_pot"));
+        }
+        if(event.getItemStack().getItem() == ModItems.ONION.get()){
+            event.getToolTip().add(Component.translatable("tooltip.farmersdelight.onion"));
+        }
+        //下面是对Minecraft原版食物进行的tooltip追加
+        if(event.getItemStack().getItem() == Items.CARROT){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.carrot"));
+        }
+        if(event.getItemStack().getItem() == Items.DRIED_KELP){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.kelp"));
+        }
+        if(event.getItemStack().getItem() == Items.POTATO){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.potato"));
+        }
+        if(event.getItemStack().getItem() == Items.PUMPKIN){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.pumpkin"));
+        }
+        if(event.getItemStack().getItem() == Items.BAMBOO){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.bamboo"));
+        }
+        if(event.getItemStack().getItem() == Items.EGG){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.egg"));
+        }
+        if(event.getItemStack().getItem() == Items.PORKCHOP){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.porkchop"));
+        }
+        if(event.getItemStack().getItem() == Items.BEEF){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.beef"));
+        }
+        if(event.getItemStack().getItem() == Items.PUFFERFISH){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.pufferfish"));
+        }
+        if(event.getItemStack().getItem() == Items.HONEY_BOTTLE){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.honey_bottle"));
+        }
+        if(event.getItemStack().getItem() == Items.SALMON){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.salmon"));
+        }
+        if(event.getItemStack().getItem() == Items.BROWN_MUSHROOM){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.mushroom"));
+        }
+        if(event.getItemStack().getItem() == Items.RED_MUSHROOM){
+            event.getToolTip().add(Component.translatable("tooltip.minecraft.mushroom"));
+        }
+
+
         //请按如下格式为物品添加tooltip
 //        if(event.getItemStack().getItem() == ...){------此处为需要添加tooltip的物品
 //            event.getToolTip().add(Component.translatable("..."));------需要为物品添加的tooltip的翻译键
