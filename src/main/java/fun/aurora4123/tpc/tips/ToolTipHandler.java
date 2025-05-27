9b2f211f98148f23bb11b79d61729152c7b9ff14
79328272 <HatsukiYukina@users.noreply.github.com>
2025-05-27 17:25:24 +0800

    提交了“烤马铃薯与金胡萝卜”的tooltip和中英文翻译。 对翻译文件进行了一次整理

--- src/main/java/fun/aurora4123/tpc/tips/ToolTipHandler.java
+++ src/main/java/fun/aurora4123/tpc/tips/ToolTipHandler.java
@@ -9,11 +9,15 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class ToolTipHandler {
     public static final Component MOD_NAME = Component.literal(TouhouPixelCanteen.MODID).withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC);
     @SubscribeEvent
     public static void setToolTip(ItemTooltipEvent event) {
+        if(event.getItemStack().getItem() == MItems.POTATO_AND_CARROT.get()){
+            event.getToolTip().add(Component.translatable("tooltip.touhoupixelcanteen.cooked_potato_and_gold_carrot"));
+        }
         //请按如下格式为物品添加tooltip
 //        if(event.getItemStack().getItem() == ...){------此处为需要添加tooltip的物品
 //            event.getToolTip().add(Component.translatable("..."));------需要为物品添加的tooltip的翻译键
